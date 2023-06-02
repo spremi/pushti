ebed9099b7fe1fcbc00feef38e857102054c51a2
Sanjeev Premi <spremi@ymail.com>
2023-06-02 23:00:44 +0530

    feat: models: Define SpX509ExtnAuthorityKeyId
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/models/sp-x509-extn-authority-key-id.ts
+++ src/app/models/sp-x509-extn-authority-key-id.ts
@@ -1,2 +1,42 @@
+//
+// @project     pushti-web
+//
+// @author      Sanjeev Premi <spremi@ymail.com>
+//
+// @license     BSD-3-Clause
+//
+// (c) Copyright 2023 Sanjeev Premi.
+//
+
+import { OctetString, fromBER } from 'asn1js';
+
+// OBJECT IDENTIFIER authorityKeyIdentifier(2 5 29 35)
+// OCTET STRING, encapsulates {
+//     SEQUENCE {
+//                 [0]
+//           AA BB CC.. ..ZZ
+
+
 export class SpX509ExtnAuthorityKeyId {
+  private keyId: Uint8Array = new Uint8Array();
+
+  public constructor() {
+  }
+
+  public setValue(value: Uint8Array): void {
+    const keyObj = fromBER(value);
+    // console.log(keyObj);
+
+    const keySeq = fromBER(keyObj.result.valueBlock.toBER())
+    // console.log(keySeq);
+
+    const keyStr = keySeq.result as OctetString;
+    // console.log(keyStr);
+
+    this.keyId = keyStr.valueBlock.valueHexView;
+  }
+
+  public getKeyId(): Uint8Array {
+    return this.keyId;
+  }
 }
