bd942ccf0f1348d6f20394c63f45f270584bd5d6
Sanjeev Premi <spremi@ymail.com>
2023-06-03 21:23:48 +0530

    chore: app: Replace template generated content
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/parts/body/body.component.ts
+++ src/app/parts/body/body.component.ts
@@ -1,3 +1,13 @@
+//
+// @project     pushti-web
+//
+// @author      Sanjeev Premi <spremi@ymail.com>
+//
+// @license     BSD-3-Clause
+//
+// (c) Copyright 2023 Sanjeev Premi.
+//
+
 import { Component } from '@angular/core';
 
 @Component({
