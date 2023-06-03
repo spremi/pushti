faedb8baeb95a5f52eeb38651fe6c78e3177be9d
Sanjeev Premi <spremi@ymail.com>
2023-06-03 21:28:11 +0530

    feat: app: routes: Add initial wiring
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,54 @@
+//
+// @project     pushti-web
+//
+// @author      Sanjeev Premi <spremi@ymail.com>
+//
+// @license     BSD-3-Clause
+//
+// (c) Copyright 2023 Sanjeev Premi.
+//
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AboutComponent } from '@routes/about/about.component';
+import { CertComponent } from '@routes/cert/cert.component';
+import { HomeComponent } from '@routes/home/home.component';
+import { LicenseComponent } from '@routes/license/license.component';
+import { PdfComponent } from '@routes/pdf/pdf.component';
+import { SettingsComponent } from '@routes/settings/settings.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'cert',
+    component: CertComponent,
+  },
+  {
+    path: 'pdf',
+    component: PdfComponent,
+  },
+  {
+    path: 'license',
+    component: LicenseComponent,
+  },
+  {
+    path: 'settings',
+    component: SettingsComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
