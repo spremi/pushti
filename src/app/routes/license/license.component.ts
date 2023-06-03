df99b19bc6a96e51a664b4edb1759613e333e8c2
Sanjeev Premi <spremi@ymail.com>
2023-06-03 23:50:34 +0530

    feat: /license: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/routes/license/license.component.ts
+++ src/app/routes/license/license.component.ts
@@ -1,4 +1,17 @@
+//
+// @project     pushti-web
+//
+// @author      Sanjeev Premi <spremi@ymail.com>
+//
+// @license     BSD-3-Clause
+//
+// (c) Copyright 2023 Sanjeev Premi.
+//
+
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { PreviousRouteService } from '@services/previous-route.service';
 
 @Component({
   selector: 'sp-license',
@@ -7,4 +20,17 @@ import { Component } from '@angular/core';
 })
 export class LicenseComponent {
 
+  constructor(private router: Router,
+    private prevRouteSvc: PreviousRouteService) {
+  }
+
+  goBack(): void {
+    const prev = this.prevRouteSvc.get();
+
+    if (prev === '') {
+      this.router.navigateByUrl('/home');
+    } else {
+      this.router.navigateByUrl(prev);
+    }
+  }
 }
