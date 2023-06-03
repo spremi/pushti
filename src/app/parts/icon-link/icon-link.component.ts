f7d35f7c3f0a77150d52c49170e8af70ca700249
Sanjeev Premi <spremi@ymail.com>
2023-06-03 21:49:07 +0530

    feat: parts: icon-link: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/parts/icon-link/icon-link.component.ts
+++ src/app/parts/icon-link/icon-link.component.ts
@@ -1,4 +1,14 @@
-import { Component } from '@angular/core';
+//
+// @project     pushti-web
+//
+// @author      Sanjeev Premi <spremi@ymail.com>
+//
+// @license     BSD-3-Clause
+//
+// (c) Copyright 2023 Sanjeev Premi.
+//
+
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'sp-icon-link',
@@ -6,5 +16,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./icon-link.component.sass']
 })
 export class IconLinkComponent {
-
+  @Input() icon = '';
+  @Input() label = '';
+  @Input() link = '';
+  @Input() disabled = false;
 }
