ea095d65a9a10b67c943d06802a8058b915b3a23
Sanjeev Premi <spremi@ymail.com>
2023-06-10 23:02:36 +0530

    chore: app: Add class 'sp-x509-extn-authority-key-id'
    Command:
      ng g class models/sp-x509-extn-authority-key-id
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ src/app/models/sp-x509-extn-authority-key-id.spec.ts
@@ -0,0 +1,7 @@
+import { SpX509ExtnAuthorityKeyId } from './sp-x509-extn-authority-key-id';
+
+describe('SpX509ExtnAuthorityKeyId', () => {
+  it('should create an instance', () => {
+    expect(new SpX509ExtnAuthorityKeyId()).toBeTruthy();
+  });
+});
