cddc521f22abb2c77733f2999833a08f29e9dd77
Sanjeev Premi <spremi@ymail.com>
2023-06-10 22:32:55 +0530

    feat: models: Define SpOidRdn
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/models/sp-oid-rdn.ts
+++ src/app/models/sp-oid-rdn.ts
@@ -1,2 +1,25 @@
-export interface SpOidRdn {
-}
+//
+// @project     pushti-web
+//
+// @author      Sanjeev Premi <spremi@ymail.com>
+//
+// @license     BSD-3-Clause
+//
+// (c) Copyright 2023 Sanjeev Premi.
+//
+
+export const SpOidRdn: Record<string, string> = {
+  '2.5.4.3': 'CN',
+  '2.5.4.4': 'SN',
+  '2.5.4.6': 'C',
+  '2.5.4.7': 'L',
+  '2.5.4.8': 'ST',
+  '2.5.4.10': 'O',
+  '2.5.4.11': 'OU',
+  '2.5.4.12': 'T',
+
+  '2.5.4.42': 'GN',
+  '2.5.4.43': 'I',
+
+  '1.2.840.113549.1.9.1': 'E-mail',
+};
