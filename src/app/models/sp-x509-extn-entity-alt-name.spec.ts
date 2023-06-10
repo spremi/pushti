a1f8df8c2368b990c837038eb70753c0c60356af
Sanjeev Premi <spremi@ymail.com>
2023-06-10 23:00:33 +0530

    chore: app: Add class 'sp-x509-extn-entity-alt-name'
    Command:
      ng g class models/sp-x509-extn-entity-alt-name
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ src/app/models/sp-x509-extn-entity-alt-name.spec.ts
@@ -0,0 +1,7 @@
+import { SpX509ExtnEntityAltName } from './sp-x509-extn-entity-alt-name';
+
+describe('SpX509ExtnEntityAltName', () => {
+  it('should create an instance', () => {
+    expect(new SpX509ExtnEntityAltName()).toBeTruthy();
+  });
+});
