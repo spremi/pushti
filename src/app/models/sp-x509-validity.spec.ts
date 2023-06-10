775b1588683acc0db63e7c1c694a1171edee839b
Sanjeev Premi <spremi@ymail.com>
2023-06-10 22:50:35 +0530

    chore: app: Add class 'sp-x509-validity'
    Command:
      ng g class models/sp-x509-validity
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ src/app/models/sp-x509-validity.spec.ts
@@ -0,0 +1,7 @@
+import { SpX509Validity } from './sp-x509-validity';
+
+describe('SpX509Validity', () => {
+  it('should create an instance', () => {
+    expect(new SpX509Validity()).toBeTruthy();
+  });
+});
