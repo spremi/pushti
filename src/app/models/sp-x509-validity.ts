643a68eef6e8677a590848d4a4a10cf5fabee9c0
Sanjeev Premi <spremi@ymail.com>
2023-06-10 23:19:55 +0530

    feat: models: Define SpX509Validity
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/models/sp-x509-validity.ts
+++ src/app/models/sp-x509-validity.ts
@@ -1,2 +1,35 @@
+//
+// @project     pushti-web
+//
+// @author      Sanjeev Premi <spremi@ymail.com>
+//
+// @license     BSD-3-Clause
+//
+// (c) Copyright 2023 Sanjeev Premi.
+//
+
 export class SpX509Validity {
+  private notBefore: Date;
+  private notAfter: Date;
+
+  public constructor() {
+    this.notBefore = new Date(0);
+    this.notAfter = new Date(0);
+  }
+
+  public getNotBefore(): Date {
+    return this.notBefore
+  }
+
+  public getNotAfter(): Date {
+    return this.notAfter
+  }
+
+  public setNotBefore(date: Date): void {
+    this.notBefore = date;
+  }
+
+  public setNotAfter(date: Date): void {
+    this.notAfter = date;
+  }
 }
