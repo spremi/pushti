55ba2eb11965c6fb6210e748dd4970e02676c306
Sanjeev Premi <spremi@ymail.com>
2023-06-10 22:53:34 +0530

    chore: app: Add class 'sp-x509-fingerprints'
    Command:
      ng g class models/sp-x509-fingerprints
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ src/app/models/sp-x509-fingerprints.spec.ts
@@ -0,0 +1,7 @@
+import { SpX509Fingerprints } from './sp-x509-fingerprints';
+
+describe('SpX509Fingerprints', () => {
+  it('should create an instance', () => {
+    expect(new SpX509Fingerprints()).toBeTruthy();
+  });
+});
