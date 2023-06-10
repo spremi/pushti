8e711c1f8c59d748c4a8c7fc5040651ad0aec597
Sanjeev Premi <spremi@ymail.com>
2023-06-10 23:03:28 +0530

    chore: app: Add class 'sp-x509-extn-cert-policy'
    Command:
      ng g class models/sp-x509-extn-cert-policy
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ src/app/models/sp-x509-extn-cert-policy.spec.ts
@@ -0,0 +1,7 @@
+import { SpX509ExtnCertPolicy } from './sp-x509-extn-cert-policy';
+
+describe('SpX509ExtnCertPolicy', () => {
+  it('should create an instance', () => {
+    expect(new SpX509ExtnCertPolicy()).toBeTruthy();
+  });
+});
