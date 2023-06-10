93f47697eeadc6dfba23088608ba28900944b149
Sanjeev Premi <spremi@ymail.com>
2023-06-10 22:49:42 +0530

    chore: app: Add class 'sp-x509-entity'
    Command:
      ng g class models/sp-x509-entity
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ src/app/models/sp-x509-entity.spec.ts
@@ -0,0 +1,7 @@
+import { SpX509Entity } from './sp-x509-entity';
+
+describe('SpX509Entity', () => {
+  it('should create an instance', () => {
+    expect(new SpX509Entity()).toBeTruthy();
+  });
+});
