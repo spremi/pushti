ca92ad9eb3e1454a80291a079c346a54f08456a1
Sanjeev Premi <spremi@ymail.com>
2023-06-10 22:57:14 +0530

    chore: app: Add class 'sp-x509-extn-basic-constraints'
    Command:
      ng g class models/sp-x509-extn-basic-constraints
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ src/app/models/sp-x509-extn-basic-constraints.spec.ts
@@ -0,0 +1,7 @@
+import { SpX509ExtnBasicConstraints } from './sp-x509-extn-basic-constraints';
+
+describe('SpX509ExtnBasicConstraints', () => {
+  it('should create an instance', () => {
+    expect(new SpX509ExtnBasicConstraints()).toBeTruthy();
+  });
+});
