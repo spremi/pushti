2e52d5a765557ffcb6daeb8ddeaedd6e3b48b98b
Sanjeev Premi <spremi@ymail.com>
2023-06-10 22:47:32 +0530

    chore: app: Add class 'sp-x509-version'
    Command:
      ng g class models/sp-x509-version
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ src/app/models/sp-x509-version.spec.ts
@@ -0,0 +1,7 @@
+import { SpX509Version } from './sp-x509-version';
+
+describe('SpX509Version', () => {
+  it('should create an instance', () => {
+    expect(new SpX509Version()).toBeTruthy();
+  });
+});
