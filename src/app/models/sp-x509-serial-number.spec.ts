9de6b9ff79eef288c38c2973c131d4672ae1a79a
Sanjeev Premi <spremi@ymail.com>
2023-06-10 22:48:34 +0530

    chore: app: Add class 'sp-x509-serial-number'
    Command:
      ng g class models/sp-x509-serial-number
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ src/app/models/sp-x509-serial-number.spec.ts
@@ -0,0 +1,7 @@
+import { SpX509SerialNumber } from './sp-x509-serial-number';
+
+describe('SpX509SerialNumber', () => {
+  it('should create an instance', () => {
+    expect(new SpX509SerialNumber()).toBeTruthy();
+  });
+});
