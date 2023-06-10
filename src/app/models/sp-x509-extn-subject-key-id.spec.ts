0e00f13272f4253351aeff71a2c4ebca86f62130
Sanjeev Premi <spremi@ymail.com>
2023-06-10 23:01:45 +0530

    chore: app: Add class 'sp-x509-extn-subject-key-id'
    Command:
      ng g class models/sp-x509-extn-subject-key-id
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ src/app/models/sp-x509-extn-subject-key-id.spec.ts
@@ -0,0 +1,7 @@
+import { SpX509ExtnSubjectKeyId } from './sp-x509-extn-subject-key-id';
+
+describe('SpX509ExtnSubjectKeyId', () => {
+  it('should create an instance', () => {
+    expect(new SpX509ExtnSubjectKeyId()).toBeTruthy();
+  });
+});
