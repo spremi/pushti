5745454ae8831958cd246a5eb9b4aeab7ca9f9e9
Sanjeev Premi <spremi@ymail.com>
2023-06-10 23:05:22 +0530

    chore: app: Add class 'sp-x509-extn-crl-distro-point'
    Command:
      ng g class models/sp-x509-extn-crl-distro-point
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ src/app/models/sp-x509-extn-crl-distro-point.spec.ts
@@ -0,0 +1,7 @@
+import { SpX509ExtnCrlDistroPoint } from './sp-x509-extn-crl-distro-point';
+
+describe('SpX509ExtnCrlDistroPoint', () => {
+  it('should create an instance', () => {
+    expect(new SpX509ExtnCrlDistroPoint()).toBeTruthy();
+  });
+});
