bcd41ef5753ae65b00426dfbce10b419f0ccacc8
Sanjeev Premi <spremi@ymail.com>
2023-06-10 23:16:58 +0530

    feat: models: Define SpX509Entity
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/models/sp-x509-entity.ts
+++ src/app/models/sp-x509-entity.ts
@@ -1,2 +1,39 @@
+//
+// @project     pushti-web
+//
+// @author      Sanjeev Premi <spremi@ymail.com>
+//
+// @license     BSD-3-Clause
+//
+// (c) Copyright 2023 Sanjeev Premi.
+//
+
+import { SpOidRdn } from './sp-oid-rdn';
+
+export interface SpX509RDN {
+  oid: string;
+  key: string,
+  value: string
+};
+
 export class SpX509Entity {
+  private rdn: SpX509RDN[];
+
+  public constructor() {
+    this.rdn = [] as SpX509RDN[];
+  }
+
+  public getRDN(): SpX509RDN[] {
+    return this.rdn;
+  }
+
+  public addRDN(argOid: string, argValue: string) {
+    const item: SpX509RDN = {
+      oid: argOid,
+      key: SpOidRdn[argOid],
+      value: argValue
+    };
+
+    this.rdn.push(item);
+  }
 }
