bf50fb2d81a7e5c5543442399dd8700fb5eb7bd8
Sanjeev Premi <spremi@ymail.com>
2023-06-10 23:09:15 +0530

    feat: models: Define SpX509Version
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/models/sp-x509-version.ts
+++ src/app/models/sp-x509-version.ts
@@ -1,2 +1,25 @@
+//
+// @project     pushti-web
+//
+// @author      Sanjeev Premi <spremi@ymail.com>
+//
+// @license     BSD-3-Clause
+//
+// (c) Copyright 2023 Sanjeev Premi.
+//
+
 export class SpX509Version {
+  private version: number;
+
+  public constructor() {
+    this.version = 0;
+  }
+
+  public get(): number {
+    return this.version;
+  }
+
+  public set(version: number): void {
+    this.version = version;
+  }
 }
