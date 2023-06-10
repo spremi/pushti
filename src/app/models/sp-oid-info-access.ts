69ee4d95d5aea3bf75df73d4fb8cbce3595534a0
Sanjeev Premi <spremi@ymail.com>
2023-06-10 22:43:12 +0530

    feat: models: Define SpOidInfoAccess
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/models/sp-oid-info-access.ts
+++ src/app/models/sp-oid-info-access.ts
@@ -1,2 +1,24 @@
-export interface SpOidInfoAccess {
+//
+// @project     pushti-web
+//
+// @author      Sanjeev Premi <spremi@ymail.com>
+//
+// @license     BSD-3-Clause
+//
+// (c) Copyright 2023 Sanjeev Premi.
+//
+
+export const SpOidInfoAccess: Record<string, string> = {
+  '1.3.6.1.5.5.7.48.1': 'Online Certificate Status Protocol (OCSP)',
+  '1.3.6.1.5.5.7.48.2': 'Certificate Authority Issuers',
+  '1.3.6.1.5.5.7.48.3': 'Time stamping',
+  '1.3.6.1.5.5.7.48.4': 'id-ad-dvcs',
+  '1.3.6.1.5.5.7.48.5': 'id-ad-caRepository',
+  '1.3.6.1.5.5.7.48.6': 'id-pkix-ocsp-archive-cutoff',
+  '1.3.6.1.5.5.7.48.7': 'id-pkix-ocsp-service-locator',
+  '1.3.6.1.5.5.7.48.8': 'id-ad-xkms',
+  '1.3.6.1.5.5.7.48.9': 'id-ad-signedObjectRepository',
+  '1.3.6.1.5.5.7.48.10': 'id-ad-rpkiManifest',
+  '1.3.6.1.5.5.7.48.11': 'id-ad-signedObject',
+  '1.3.6.1.5.5.7.48.12': 'id-ad-cmc',
 }
