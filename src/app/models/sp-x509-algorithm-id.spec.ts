46fde51d7f3ac38a14deacda53a0122484f2ceec
Sanjeev Premi <spremi@ymail.com>
2023-06-10 22:51:18 +0530

    chore: app: Add class 'sp-x509-algorithm-id'
    Command:
      ng g class models/sp-x509-algorithm-id
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ src/app/models/sp-x509-algorithm-id.spec.ts
@@ -0,0 +1,7 @@
+import { SpX509AlgorithmId } from './sp-x509-algorithm-id';
+
+describe('SpX509AlgorithmId', () => {
+  it('should create an instance', () => {
+    expect(new SpX509AlgorithmId()).toBeTruthy();
+  });
+});
