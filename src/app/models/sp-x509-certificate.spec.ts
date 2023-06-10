187c26f9370f60b7a92a51eb2504f60c82ed6dd3
Sanjeev Premi <spremi@ymail.com>
2023-06-10 23:06:13 +0530

    chore: app: Add class 'sp-x509-certificate'
    Command:
      ng g class models/sp-x509-certificate
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ src/app/models/sp-x509-certificate.spec.ts
@@ -0,0 +1,7 @@
+import { SpX509Certificate } from './sp-x509-certificate';
+
+describe('SpX509Certificate', () => {
+  it('should create an instance', () => {
+    expect(new SpX509Certificate()).toBeTruthy();
+  });
+});
