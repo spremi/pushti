569c872bb202d2bfc63af7e2cca1de3eeaedcdcd
Sanjeev Premi <spremi@ymail.com>
2023-06-10 23:04:19 +0530

    chore: app: Add class 'sp-x509-extn-authority-info-access'
    Command:
      ng g class models/sp-x509-extn-authority-info-access
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ src/app/models/sp-x509-extn-authority-info-access.spec.ts
@@ -0,0 +1,7 @@
+import { SpX509ExtnAuthorityInfoAccess } from './sp-x509-extn-authority-info-access';
+
+describe('SpX509ExtnAuthorityInfoAccess', () => {
+  it('should create an instance', () => {
+    expect(new SpX509ExtnAuthorityInfoAccess()).toBeTruthy();
+  });
+});
