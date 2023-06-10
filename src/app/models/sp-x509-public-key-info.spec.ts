149aadbabe97f6a257a93f4fdd9865c8a3e7acdf
Sanjeev Premi <spremi@ymail.com>
2023-06-10 22:52:24 +0530

    chore: app: Add class 'sp-x509-public-key-info'
    Command:
      ng g class models/sp-x509-public-key-info
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ src/app/models/sp-x509-public-key-info.spec.ts
@@ -0,0 +1,7 @@
+import { SpX509PublicKeyInfo } from './sp-x509-public-key-info';
+
+describe('SpX509PublicKeyInfo', () => {
+  it('should create an instance', () => {
+    expect(new SpX509PublicKeyInfo()).toBeTruthy();
+  });
+});
