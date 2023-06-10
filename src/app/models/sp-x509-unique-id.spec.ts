51b4034e263298d38e1d60fcd8c68926b6a5c235
Sanjeev Premi <spremi@ymail.com>
2023-06-10 22:55:26 +0530

    chore: app: Add class 'sp-x509-unique-id'
    Command:
      ng g class models/sp-x509-unique-id
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ src/app/models/sp-x509-unique-id.spec.ts
@@ -0,0 +1,7 @@
+import { SpX509UniqueId } from './sp-x509-unique-id';
+
+describe('SpX509UniqueId', () => {
+  it('should create an instance', () => {
+    expect(new SpX509UniqueId()).toBeTruthy();
+  });
+});
