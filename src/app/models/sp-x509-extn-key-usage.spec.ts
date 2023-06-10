729d7b5448ea78855b0602f47ac35c387f9c5eb2
Sanjeev Premi <spremi@ymail.com>
2023-06-10 22:58:08 +0530

    chore: app: Add class 'sp-x509-extn-key-usage'
    Command:
      ng g class models/sp-x509-extn-key-usage
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ src/app/models/sp-x509-extn-key-usage.spec.ts
@@ -0,0 +1,7 @@
+import { SpX509ExtnKeyUsage } from './sp-x509-extn-key-usage';
+
+describe('SpX509ExtnKeyUsage', () => {
+  it('should create an instance', () => {
+    expect(new SpX509ExtnKeyUsage()).toBeTruthy();
+  });
+});
