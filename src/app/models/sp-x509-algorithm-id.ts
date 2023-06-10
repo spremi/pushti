9fe6751b2cb246e63884d5f53ea09f51e17d666f
Sanjeev Premi <spremi@ymail.com>
2023-06-10 23:23:26 +0530

    feat: models: Define SpX509AlgorithmId
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/models/sp-x509-algorithm-id.ts
+++ src/app/models/sp-x509-algorithm-id.ts
@@ -1,2 +1,34 @@
+//
+// @project     pushti-web
+//
+// @author      Sanjeev Premi <spremi@ymail.com>
+//
+// @license     BSD-3-Clause
+//
+// (c) Copyright 2023 Sanjeev Premi.
+//
+
+import { SpOidAlgorithm } from './sp-oid-algorithm';
+
 export class SpX509AlgorithmId {
+  private oid: string;
+  private name: string;
+
+  public constructor() {
+    this.oid = '';
+    this.name = '';
+  }
+
+  public getOid(): string {
+    return this.oid;
+  }
+
+  public getName(): string {
+    return this.name;
+  }
+
+  public setOid(oid: string): void {
+    this.oid = oid;
+    this.name = SpOidAlgorithm[oid];
+  }
 }
