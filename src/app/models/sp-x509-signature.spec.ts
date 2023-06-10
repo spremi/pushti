68e0f8c55ef2a7754ec4fc7ca4a6aeb29bc7e114
Sanjeev Premi <spremi@ymail.com>
2023-06-10 22:54:32 +0530

    chore: app: Add class 'sp-x509-signature'
    Command:
      ng g class models/sp-x509-signature
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ src/app/models/sp-x509-signature.spec.ts
@@ -0,0 +1,7 @@
+import { SpX509Signature } from './sp-x509-signature';
+
+describe('SpX509Signature', () => {
+  it('should create an instance', () => {
+    expect(new SpX509Signature()).toBeTruthy();
+  });
+});
