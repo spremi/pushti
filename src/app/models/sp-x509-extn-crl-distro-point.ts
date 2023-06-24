e092519c4cde5eae6bdd3490a0b3f7410a509fcc
Sanjeev Premi <spremi@ymail.com>
2023-06-24 23:07:28 +0530

    feat: models: Define SpX509ExtnCrlDistroPoint
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/models/sp-x509-extn-crl-distro-point.ts
+++ src/app/models/sp-x509-extn-crl-distro-point.ts
@@ -1,2 +1,47 @@
+//
+// @project     pushti-web
+//
+// @author      Sanjeev Premi <spremi@ymail.com>
+//
+// @license     BSD-3-Clause
+//
+// (c) Copyright 2023 Sanjeev Premi.
+//
+
+import { CRLDistributionPoints } from 'pkijs';
+
+/**
+ * Local definition for object received after parsing CRL Distribution Points.
+ */
+interface LocalCrlObject {
+  type: number;
+  value: string;
+}
+
 export class SpX509ExtnCrlDistroPoint {
+
+  private urls: string[] = [];
+
+  public constructor() {
+  }
+
+  public setValue(value: Uint8Array): void {
+    this.urls = [];
+
+    const cdp = CRLDistributionPoints.fromBER(value);
+
+    cdp.distributionPoints.forEach(p => {
+      if (p.distributionPoint) {
+        const crlObjects = p.distributionPoint as Array<LocalCrlObject>;
+
+        crlObjects.forEach(obj => {
+          this.urls.push(obj.value);
+        });
+      }
+    });
+  }
+
+  public getUrls(): string[] {
+    return this.urls;
+  }
 }
