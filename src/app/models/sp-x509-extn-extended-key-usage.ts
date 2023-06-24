254528db31ec3d965fb9a0c423a9841fd1f86ed0
Sanjeev Premi <spremi@ymail.com>
2023-06-24 22:42:45 +0530

    feat: models: Define SpX509ExtnExtendedKeyUsage
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/models/sp-x509-extn-extended-key-usage.ts
+++ src/app/models/sp-x509-extn-extended-key-usage.ts
@@ -1,2 +1,38 @@
+//
+// @project     pushti-web
+//
+// @author      Sanjeev Premi <spremi@ymail.com>
+//
+// @license     BSD-3-Clause
+//
+// (c) Copyright 2023 Sanjeev Premi.
+//
+
+import { SpOidExtendedKeyUsage } from '@models/sp-oid-extended-key-usage';
+import { ExtKeyUsage } from 'pkijs';
+
 export class SpX509ExtnExtendedKeyUsage {
+
+  usage: string[] = [];
+
+  public constructor() {
+  }
+
+  public setValue(value: Uint8Array): void {
+    this.usage = [];
+
+    const keyUsage = ExtKeyUsage.fromBER(value);
+
+    keyUsage.keyPurposes.forEach(purpose => {
+      if (purpose in SpOidExtendedKeyUsage) {
+        this.usage.push(SpOidExtendedKeyUsage[purpose]);
+      } else {
+        this.usage.push('See OID: ' + purpose);
+      }
+    });
+  }
+
+  public getUsage(): string[] {
+    return this.usage;
+  }
 }
