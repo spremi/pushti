62588025c67dccdfb429b66fee97d6bcbefc67d3
Sanjeev Premi <spremi@ymail.com>
2023-06-25 02:21:16 +0530

    feat: pipes: to-str: Basic implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/pipes/to-str.pipe.ts
+++ src/app/pipes/to-str.pipe.ts
@@ -1,3 +1,13 @@
+//
+// @project     pushti-web
+//
+// @author      Sanjeev Premi <spremi@ymail.com>
+//
+// @license     BSD-3-Clause
+//
+// (c) Copyright 2023 Sanjeev Premi.
+//
+
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
@@ -5,8 +15,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ToStrPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
-  }
+  transform(buf: Uint8Array): string {
+    let ret = Array.from(buf)
+      .map(item => String.fromCharCode(item))
+      .join('');
 
+    return ret;
+  }
 }
