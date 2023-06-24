0ed416162bb1a703a4d6d3c4d5b9ee18145e360b
Sanjeev Premi <spremi@ymail.com>
2023-06-25 01:53:10 +0530

    feat: models: Define SpX509ExtnBasicConstraints
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/models/sp-x509-extn-basic-constraints.ts
+++ src/app/models/sp-x509-extn-basic-constraints.ts
@@ -1,2 +1,30 @@
+//
+// @project     pushti-web
+//
+// @author      Sanjeev Premi <spremi@ymail.com>
+//
+// @license     BSD-3-Clause
+//
+// (c) Copyright 2023 Sanjeev Premi.
+//
+
+import { BasicConstraints } from 'pkijs';
+
 export class SpX509ExtnBasicConstraints {
+
+  private ca = false;
+
+  public setValue(value: Uint8Array): void {
+    if (value) {
+      const bc = BasicConstraints.fromBER(value);
+
+      this.ca = bc.cA;
+    } else {
+      this.ca = false;
+    }
+  }
+
+  public isCA(): boolean {
+    return this.ca;
+  }
 }
