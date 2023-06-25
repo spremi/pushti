76672ed2ce874a66cb5cad340916edddef7714e4
Sanjeev Premi <spremi@ymail.com>
2023-06-25 16:36:22 +0530

    feat: models: Define supported mime types
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/models/file-mimes.ts
+++ src/app/models/file-mimes.ts
@@ -1,2 +1,18 @@
-export interface FileMimes {
-}
+//
+// @project     pushti-web
+//
+// @author      Sanjeev Premi <spremi@ymail.com>
+//
+// @license     BSD-3-Clause
+//
+// (c) Copyright 2023 Sanjeev Premi.
+//
+
+export const FileMimesCert = [
+  'application/x-x509-ca-cert',
+  'text/plain'
+];
+
+export const FileMimesPdf = [
+  'application/pdf',
+];
