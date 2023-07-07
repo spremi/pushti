57f483e099ed48c9bbe55711a0230d222a8753e8
Sanjeev Premi <spremi@ymail.com>
2023-07-08 02:53:41 +0530

    feat: parts: sp-show-entity: Basic implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/parts/show-entity/show-entity.component.ts
+++ src/app/parts/show-entity/show-entity.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+//
+// @project     pushti-web
+//
+// @author      Sanjeev Premi <spremi@ymail.com>
+//
+// @license     BSD-3-Clause
+//
+// (c) Copyright 2023 Sanjeev Premi.
+//
+
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { SpX509Entity } from '@models/sp-x509-entity';
+
+interface KeyValue {
+  [key: string]: string;
+}
 
 @Component({
   selector: 'sp-show-entity',
   templateUrl: './show-entity.component.html',
   styleUrls: ['./show-entity.component.sass']
 })
-export class ShowEntityComponent {
+export class ShowEntityComponent implements OnChanges {
+  @Input() title!: string;
+  @Input() value!: SpX509Entity;
 
+  rdn: KeyValue = {};
+
+  ngOnChanges(changes: SimpleChanges): void {
+    for (const attr in changes) {
+      if (attr === 'value') {
+        this.value = changes[attr].currentValue;
+
+        if (this.value) {
+          this.value.getRDN().forEach(dn => {
+            this.rdn[dn.key] = dn.value;
+          });
+        } else {
+          this.rdn = {};
+        }
+      }
+    }
+  }
+
+  onCopy(): void {
+    console.log('Copy clicked!');
+  }
 }
