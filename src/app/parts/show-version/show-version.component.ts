06ec725dc854bb95e35a2d3ef6f8d7a9dfc97fd6
Sanjeev Premi <spremi@ymail.com>
2023-07-08 02:11:27 +0530

    feat: parts: show-version: Basic implementation
    Also includes generic style definitions.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/parts/show-version/show-version.component.ts
+++ src/app/parts/show-version/show-version.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+//
+// @project     pushti-web
+//
+// @author      Sanjeev Premi <spremi@ymail.com>
+//
+// @license     BSD-3-Clause
+//
+// (c) Copyright 2023 Sanjeev Premi.
+//
+
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { SpX509Version } from '@models/sp-x509-version';
 
 @Component({
   selector: 'sp-show-version',
   templateUrl: './show-version.component.html',
   styleUrls: ['./show-version.component.sass']
 })
-export class ShowVersionComponent {
+export class ShowVersionComponent implements OnChanges {
+  @Input() value!: SpX509Version;
 
+  version = 1;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    for (const attr in changes) {
+      if (attr === 'value') {
+        this.value = changes[attr].currentValue;
+
+        if (this.value) {
+          this.version = this.value.get();
+        } else {
+          this.version = 1;
+        }
+      }
+    }
+  }
+
+  onCopy(): void {
+    console.log('Copy clicked!');
+  }
 }
