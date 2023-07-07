4d352f29babb7b3c802ee87e147a8c6b5dc121bf
Sanjeev Premi <spremi@ymail.com>
2023-07-08 02:43:23 +0530

    feat: parts: sp-show-validity: Basic implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/parts/show-validity/show-validity.component.ts
+++ src/app/parts/show-validity/show-validity.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+//
+// @project     pushti-web
+//
+// @author      Sanjeev Premi <spremi@ymail.com>
+//
+// @license     BSD-3-Clause
+//
+// (c) Copyright 2023 Sanjeev Premi.
+//
+
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { SpX509Validity } from '@models/sp-x509-validity';
 
 @Component({
   selector: 'sp-show-validity',
   templateUrl: './show-validity.component.html',
   styleUrls: ['./show-validity.component.sass']
 })
-export class ShowValidityComponent {
+export class ShowValidityComponent implements OnChanges {
+  @Input() value!: SpX509Validity;
 
+  notBefore = new Date(0);
+  notAfter = new Date(0);
+
+  ngOnChanges(changes: SimpleChanges): void {
+    for (const attr in changes) {
+      if (attr === 'value') {
+        this.value = changes[attr].currentValue;
+
+        if (this.value) {
+          this.notBefore = this.value.getNotBefore();
+          this.notAfter = this.value.getNotBefore();
+        } else {
+          this.notBefore = new Date(0);
+          this.notAfter = new Date(0);
+        }
+      }
+    }
+  }
+
+  onCopy(): void {
+    console.log('Copy clicked!');
+  }
 }
