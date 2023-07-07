b3c5a6a1e71e0adc0f872cf586006357b9376a47
Sanjeev Premi <spremi@ymail.com>
2023-07-08 01:41:51 +0530

    feat: parts: box: Implement basic bounding box
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/parts/box/box.component.ts
+++ src/app/parts/box/box.component.ts
@@ -1,4 +1,14 @@
-import { Component } from '@angular/core';
+//
+// @project     pushti-web
+//
+// @author      Sanjeev Premi <spremi@ymail.com>
+//
+// @license     BSD-3-Clause
+//
+// (c) Copyright 2023 Sanjeev Premi.
+//
+
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'sp-box',
@@ -6,5 +16,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./box.component.sass']
 })
 export class BoxComponent {
+  @Input() title = '';
+  @Input() oid = '';
+  @Output() copy = new EventEmitter<any>();
 
+  onCopyContent(): void {
+    this.copy.emit();
+  }
 }
