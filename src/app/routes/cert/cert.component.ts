887dedfdcbfd2e0ceca7bd5af8f49f2f92ddd4e0
Sanjeev Premi <spremi@ymail.com>
2023-07-09 01:25:39 +0530

    feat: /cert: Initial implementation
    Includes:
     . Toolbar to show name of certificate file, if available.
     . Button to open file selection dialog.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/routes/cert/cert.component.ts
+++ src/app/routes/cert/cert.component.ts
@@ -1,4 +1,16 @@
-import { Component } from '@angular/core';
+//
+// @project     pushti-web
+//
+// @author      Sanjeev Premi <spremi@ymail.com>
+//
+// @license     BSD-3-Clause
+//
+// (c) Copyright 2023 Sanjeev Premi.
+//
+
+import { Component, inject } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { FileStoreService } from '@services/file-store.service';
 
 @Component({
   selector: 'sp-cert',
@@ -7,4 +19,54 @@ import { Component } from '@angular/core';
 })
 export class CertComponent {
 
+  private fileStore = inject(FileStoreService);
+  private snackBar = inject(MatSnackBar);
+
+  readonly ACCEPT_EXTENSIONS = [
+    '.ber',
+    '.cer',
+    '.crt',
+    '.der',
+    '.pem',
+    '.txt',
+  ].join(',');
+
+  certFile = '';
+
+  /**
+   * Handle the file select event.
+   */
+  onFileSelected(event: Event): void {
+    const target = event.target as HTMLInputElement;
+    const files = target.files as FileList;
+
+    // Attribute 'multiple' is set to false.
+    // We can safely use index 0 get file object.
+    const file = files[0];
+
+    const fileName = file.name;
+    const fileType = file.type;
+
+    console.log('Reading file ' + fileName + ' as ' + fileType);
+
+    const reader = new FileReader();
+
+    let mime = file.type;
+
+    reader.onload = () => {
+      let ret = this.fileStore.save(fileName, fileType, reader.result as string);
+
+      if (ret) {
+        this.certFile = fileName;
+      } else {
+        this.snackBar.open('Unable to parse file: ' + fileName);
+      }
+    };
+
+    reader.onerror = () => {
+      this.snackBar.open('Unable to read file: ' + fileName + '\n' + reader.error?.message as string);
+    };
+
+    reader.readAsBinaryString(file);
+  }
 }
