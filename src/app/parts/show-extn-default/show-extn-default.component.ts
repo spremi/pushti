e2c874bc3c924235d49e127ad042a3dbfea5fe50
Sanjeev Premi <spremi@ymail.com>
2023-07-16 13:24:45 +0530

    feat: parts: show-extn-default: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/parts/show-extn-default/show-extn-default.component.ts
+++ src/app/parts/show-extn-default/show-extn-default.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+//
+// @project     pushti-web
+//
+// @author      Sanjeev Premi <spremi@ymail.com>
+//
+// @license     BSD-3-Clause
+//
+// (c) Copyright 2023 Sanjeev Premi.
+//
+
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'sp-show-extn-default',
   templateUrl: './show-extn-default.component.html',
   styleUrls: ['./show-extn-default.component.sass']
 })
-export class ShowExtnDefaultComponent {
+export class ShowExtnDefaultComponent implements OnChanges {
+  @Input() oid = '';
+  @Input() critical = false;
+  @Input() value!: Uint8Array;
 
+  ngOnChanges(changes: SimpleChanges): void {
+    for (const attr in changes) {
+      switch (attr) {
+        case 'oid':
+          this.oid = changes[attr].currentValue;
+          break;
+
+        case 'critical':
+          this.critical = changes[attr].currentValue;
+          break;
+
+        case 'value':
+          this.value = changes[attr].currentValue;
+          break;
+
+        default:
+          break;
+      }
+    }
+  }
+
+  onCopy(): void {
+  }
 }
