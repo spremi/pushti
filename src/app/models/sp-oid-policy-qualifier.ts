e8c70bae6a7db7c820c4c923b8e50db885dbe71d
Sanjeev Premi <spremi@ymail.com>
2023-08-05 02:06:53 +0530

    feat: models: Define SpOidPolicyQualifier
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/models/sp-oid-policy-qualifier.ts
+++ src/app/models/sp-oid-policy-qualifier.ts
@@ -1,2 +1,17 @@
-export interface SpOidPolicyQualifier {
-}
+//
+// @project     pushti-web
+//
+// @author      Sanjeev Premi <spremi@ymail.com>
+//
+// @license     BSD-3-Clause
+//
+// (c) Copyright 2023 Sanjeev Premi.
+//
+
+export const SpOidPolicyQualifier: Record<string, string> = {
+  '1.3.6.1.5.5.7.2.1': 'CPS Pointer Qualifier',
+  '1.3.6.1.5.5.7.2.2': 'User Notice Qualifier',
+  '1.3.6.1.5.5.7.2.3': 'Reserved and Obsolete',
+  '1.3.6.1.5.5.7.2.4': 'Attribute Certificate (AC) Pointer Qualifier',  // RFC4476
+  '1.3.6.1.5.5.7.2.5': 'Attribute Certificate (AC) User Notice',  // RFC4476
+};
