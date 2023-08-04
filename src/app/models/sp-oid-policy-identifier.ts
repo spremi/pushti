206948fa46439bfc9af463cc7ae549a1260425d3
Sanjeev Premi <spremi@ymail.com>
2023-08-05 02:08:04 +0530

    feat: models: Define SpOidPolicyIdentifier
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/models/sp-oid-policy-identifier.ts
+++ src/app/models/sp-oid-policy-identifier.ts
@@ -1,2 +1,13 @@
-export interface SpOidPolicyIdentifier {
-}
+//
+// @project     pushti-web
+//
+// @author      Sanjeev Premi <spremi@ymail.com>
+//
+// @license     BSD-3-Clause
+//
+// (c) Copyright 2023 Sanjeev Premi.
+//
+
+export const SpOidPolicyIdentifier: Record<string, string> = {
+  '2.5.29.32.0': 'Any Policy',
+};
