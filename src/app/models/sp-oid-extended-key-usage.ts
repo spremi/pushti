e0fd686086620c1687dda204a90ff4a1354ef3b4
Sanjeev Premi <spremi@ymail.com>
2023-08-05 02:04:47 +0530

    feat: models: Define SpOidExtendedKeyUsage
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/models/sp-oid-extended-key-usage.ts
+++ src/app/models/sp-oid-extended-key-usage.ts
@@ -1,2 +1,18 @@
-export interface SpOidExtendedKeyUsage {
-}
+//
+// @project     pushti-web
+//
+// @author      Sanjeev Premi <spremi@ymail.com>
+//
+// @license     BSD-3-Clause
+//
+// (c) Copyright 2023 Sanjeev Premi.
+//
+
+export const SpOidExtendedKeyUsage: Record<string, string> = {
+  '1.3.6.1.5.5.7.3.1': 'TLS WWW server authentication',
+  '1.3.6.1.5.5.7.3.2': 'TLS WWW client authentication',
+  '1.3.6.1.5.5.7.3.3': 'Signing of downloadable executable code',
+  '1.3.6.1.5.5.7.3.4': 'Email protection',
+  '1.3.6.1.5.5.7.3.8': 'Timestamping',
+  '1.3.6.1.5.5.7.3.9': 'Signing OCSP responses',
+};
