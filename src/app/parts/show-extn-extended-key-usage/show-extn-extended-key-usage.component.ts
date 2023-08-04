7e3412724e83b237a051aa1ba46d5a4dbc958194
Sanjeev Premi <spremi@ymail.com>
2023-08-05 02:05:12 +0530

    feat: parts: show-extn-extended-key-usage: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/parts/show-extn-extended-key-usage/show-extn-extended-key-usage.component.ts
+++ src/app/parts/show-extn-extended-key-usage/show-extn-extended-key-usage.component.ts
@@ -1,4 +1,15 @@
-import { Component } from '@angular/core';
+//
+// @project     pushti-web
+//
+// @author      Sanjeev Premi <spremi@ymail.com>
+//
+// @license     BSD-3-Clause
+//
+// (c) Copyright 2023 Sanjeev Premi.
+//
+
+import { Component, Input, SimpleChanges } from '@angular/core';
+import { SpX509ExtnExtendedKeyUsage } from '@models/sp-x509-extn-extended-key-usage';
 
 @Component({
   selector: 'sp-show-extn-extended-key-usage',
@@ -6,5 +17,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./show-extn-extended-key-usage.component.sass']
 })
 export class ShowExtnExtendedKeyUsageComponent {
+  @Input() oid = '';
+  @Input() critical = false;
+  @Input() value!: Uint8Array;
 
+  usage: string[] = [];
+
+  ngOnChanges(changes: SimpleChanges): void {
+    for (const attr in changes) {
+      if (attr === 'value') {
+        this.value = changes[attr].currentValue;
+
+        if (this.value) {
+          const keyUsage = new SpX509ExtnExtendedKeyUsage();
+          keyUsage.setValue(this.value);
+
+          this.usage = keyUsage.getUsage();
+        } else {
+          this.usage = [];
+        }
+      }
+    }
+  }
+
+  onCopy(): void {
+  }
 }
