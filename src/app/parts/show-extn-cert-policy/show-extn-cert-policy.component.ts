ce93561fe414139cc545cf75453dd0b91f598237
Sanjeev Premi <spremi@ymail.com>
2023-08-05 13:16:34 +0530

    feat: parts: show-extn-cert-policy: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/parts/show-extn-cert-policy/show-extn-cert-policy.component.ts
+++ src/app/parts/show-extn-cert-policy/show-extn-cert-policy.component.ts
@@ -1,4 +1,15 @@
-import { Component } from '@angular/core';
+//
+// @project     pushti-web
+//
+// @author      Sanjeev Premi <spremi@ymail.com>
+//
+// @license     BSD-3-Clause
+//
+// (c) Copyright 2023 Sanjeev Premi.
+//
+
+import { Component, Input, SimpleChanges } from '@angular/core';
+import { SpCertificatePolicy, SpX509ExtnCertPolicy } from '@models/sp-x509-extn-cert-policy';
 
 @Component({
   selector: 'sp-show-extn-cert-policy',
@@ -6,5 +17,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./show-extn-cert-policy.component.sass']
 })
 export class ShowExtnCertPolicyComponent {
+  @Input() oid = '';
+  @Input() critical = false;
+  @Input() value!: Uint8Array;
 
+  policies: SpCertificatePolicy[] = [];
+
+  ngOnChanges(changes: SimpleChanges): void {
+    for (const attr in changes) {
+      if (attr === 'value') {
+        this.value = changes[attr].currentValue;
+
+        this.parse();
+      }
+    }
+  }
+
+  onCopy(): void {
+  }
+
+  private parse(): void {
+    if (this.value) {
+      const policy = new SpX509ExtnCertPolicy();
+      policy.setValue(this.value);
+
+      this.policies = policy.getPolicies();
+
+    } else {
+      this.policies = [];
+    }
+  }
 }
