2b37ddfe0293b9eee76947348b8a50ac0f54adee
Sanjeev Premi <spremi@ymail.com>
2023-08-05 13:21:16 +0530

    feat: parts: show-extn-issuer-alt-name: Basic implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/parts/show-extn-issuer-alt-name/show-extn-issuer-alt-name.component.ts
+++ src/app/parts/show-extn-issuer-alt-name/show-extn-issuer-alt-name.component.ts
@@ -1,4 +1,15 @@
-import { Component } from '@angular/core';
+//
+// @project     pushti-web
+//
+// @author      Sanjeev Premi <spremi@ymail.com>
+//
+// @license     BSD-3-Clause
+//
+// (c) Copyright 2023 Sanjeev Premi.
+//
+
+import { Component, Input, SimpleChanges } from '@angular/core';
+import { SpX509ExtnEntityAltName } from '@models/sp-x509-extn-entity-alt-name';
 
 @Component({
   selector: 'sp-show-extn-issuer-alt-name',
@@ -6,5 +17,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./show-extn-issuer-alt-name.component.sass']
 })
 export class ShowExtnIssuerAltNameComponent {
+  @Input() oid = '';
+  @Input() critical = false;
+  @Input() value!: Uint8Array;
 
+  names!: Uint8Array[];
+
+  ngOnChanges(changes: SimpleChanges): void {
+    for (const attr in changes) {
+      if (attr === 'value') {
+        this.value = changes[attr].currentValue;
+
+        if (this.value) {
+          const altName = new SpX509ExtnEntityAltName();
+          altName.setValue(this.value);
+
+          this.names = altName.getNames();
+        } else {
+          this.names = [];
+        }
+      }
+    }
+  }
+
+  onCopy(): void {
+  }
 }
