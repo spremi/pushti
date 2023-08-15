c73ccc66ae73db657b53c2a91cce40b9c7fcc5f6
Sanjeev Premi <spremi@ymail.com>
2023-08-15 16:55:15 +0530

    feat: models: Define SpX509ExtnNsComment
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/models/sp-x509-extn-ns-comment.ts
+++ src/app/models/sp-x509-extn-ns-comment.ts
@@ -1,2 +1,27 @@
+//
+// @project     pushti-web
+//
+// @author      Sanjeev Premi <spremi@ymail.com>
+//
+// @license     BSD-3-Clause
+//
+// (c) Copyright 2023 Sanjeev Premi.
+//
+
+import { IA5String, fromBER } from 'asn1js';
+
 export class SpX509ExtnNsComment {
+  private comment = '';
+
+  public setValue(value: Uint8Array): void {
+    const obj1 = fromBER(value);
+
+    const obj2 = obj1.result as IA5String;
+
+    this.comment = obj2.getValue();
+  }
+
+  public getComment(): string {
+    return this.comment;
+  }
 }
