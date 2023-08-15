f2b8bdd17a4ffae6fa29e7798d60d34c6c9ec2ed
Sanjeev Premi <spremi@ymail.com>
2023-08-15 16:51:05 +0530

    chore: app: Add class 'sp-x509-extn-ns-comment'
    Command:
      ng g class models/sp-x509-extn-ns-comment
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ src/app/models/sp-x509-extn-ns-comment.spec.ts
@@ -0,0 +1,7 @@
+import { SpX509ExtnNsComment } from './sp-x509-extn-ns-comment';
+
+describe('SpX509ExtnNsComment', () => {
+  it('should create an instance', () => {
+    expect(new SpX509ExtnNsComment()).toBeTruthy();
+  });
+});
