139b9504d6a5bdc02c7cd0cbaeaf599c48d43ccf
Sanjeev Premi <spremi@ymail.com>
2023-08-15 17:02:36 +0530

    feat: parts: sp-show-extn-ns-comment: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/parts/show-extn-ns-comment/show-extn-ns-comment.component.ts
+++ src/app/parts/show-extn-ns-comment/show-extn-ns-comment.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+//
+// @project     pushti-web
+//
+// @author      Sanjeev Premi <spremi@ymail.com>
+//
+// @license     BSD-3-Clause
+//
+// (c) Copyright 2023 Sanjeev Premi.
+//
+
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { SpX509ExtnNsComment } from '@models/sp-x509-extn-ns-comment';
 
 @Component({
   selector: 'sp-show-extn-ns-comment',
   templateUrl: './show-extn-ns-comment.component.html',
   styleUrls: ['./show-extn-ns-comment.component.sass']
 })
-export class ShowExtnNsCommentComponent {
+export class ShowExtnNsCommentComponent implements OnChanges {
+  @Input() oid = '';
+  @Input() critical = false;
+  @Input() value!: Uint8Array;
 
+  comment = '';
+
+  ngOnChanges(changes: SimpleChanges): void {
+    for (const attr in changes) {
+      if (attr === 'value') {
+        this.value = changes[attr].currentValue;
+
+        if (this.value) {
+          const nsComment = new SpX509ExtnNsComment();
+          nsComment.setValue(this.value);
+
+          this.comment = nsComment.getComment();
+
+        } else {
+          this.comment = '';
+        }
+      }
+    }
+  }
+
+  onCopy(): void {
+  }
 }
