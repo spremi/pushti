286312dc046211d8269f9ddb870285b330315e44
Sanjeev Premi <spremi@ymail.com>
2023-08-20 00:11:22 +0530

    feat: parts: show-fingerprints: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/parts/show-fingerprints/show-fingerprints.component.ts
+++ src/app/parts/show-fingerprints/show-fingerprints.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+//
+// @project     pushti-web
+//
+// @author      Sanjeev Premi <spremi@ymail.com>
+//
+// @license     BSD-3-Clause
+//
+// (c) Copyright 2023 Sanjeev Premi.
+//
+
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { SpX509Fingerprints } from '@models/sp-x509-fingerprints';
 
 @Component({
   selector: 'sp-show-fingerprints',
   templateUrl: './show-fingerprints.component.html',
   styleUrls: ['./show-fingerprints.component.sass']
 })
-export class ShowFingerprintsComponent {
+export class ShowFingerprintsComponent implements OnChanges {
+  @Input() value!: SpX509Fingerprints;
 
+  ngOnChanges(changes: SimpleChanges): void {
+    for (const attr in changes) {
+      if (attr === 'value') {
+        this.value = changes[attr].currentValue;
+      }
+    }
+  }
+
+  onCopy(): void {
+    console.log('Copy clicked!');
+  }
 }
