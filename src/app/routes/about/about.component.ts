86ec1b66120c0493746bf158f98c345abf2d094f
Sanjeev Premi <spremi@ymail.com>
2023-08-20 00:02:07 +0530

    feat: /about: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/routes/about/about.component.ts
+++ src/app/routes/about/about.component.ts
@@ -1,4 +1,16 @@
-import { Component } from '@angular/core';
+//
+// @project     pushti-web
+//
+// @author      Sanjeev Premi <spremi@ymail.com>
+//
+// @license     BSD-3-Clause
+//
+// (c) Copyright 2023 Sanjeev Premi.
+//
+
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { PreviousRouteService } from '@services/previous-route.service';
 
 @Component({
   selector: 'sp-about',
@@ -6,5 +18,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./about.component.sass']
 })
 export class AboutComponent {
+  private router = inject(Router);
+  private prevRouteSvc = inject(PreviousRouteService);
 
+  onDismiss(): void {
+    const prev = this.prevRouteSvc.get();
+
+    if (prev === null) {
+      this.router.navigateByUrl('/home');
+    } else {
+      this.router.navigateByUrl(prev);
+    }
+  }
 }
