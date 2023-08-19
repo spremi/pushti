3088d8ecf2b098ac23cdbf68501a9364ef8b07a3
Sanjeev Premi <spremi@ymail.com>
2023-08-20 00:20:29 +0530

    feat: parts: show-signature: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/parts/show-signature/show-signature.component.ts
+++ src/app/parts/show-signature/show-signature.component.ts
@@ -1,4 +1,15 @@
-import { Component } from '@angular/core';
+//
+// @project     pushti-web
+//
+// @author      Sanjeev Premi <spremi@ymail.com>
+//
+// @license     BSD-3-Clause
+//
+// (c) Copyright 2023 Sanjeev Premi.
+//
+
+import { Component, Input, SimpleChanges } from '@angular/core';
+import { SpX509Signature } from '@models/sp-x509-signature';
 
 @Component({
   selector: 'sp-show-signature',
@@ -6,5 +17,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./show-signature.component.sass']
 })
 export class ShowSignatureComponent {
+  @Input() value!: SpX509Signature;
+
+  signAlgo = '';
+  signValue = new Uint8Array();
+
+  ngOnChanges(changes: SimpleChanges): void {
+    for (const attr in changes) {
+      if (attr === 'value') {
+        this.value = changes[attr].currentValue;
+
+        if (this.value) {
+          this.signAlgo = this.value.getAlgorithmId().getName();
+          this.signValue = this.value.getValue();
+        } else {
+          this.signAlgo = '';
+          this.signValue = new Uint8Array();
+        }
+      }
+    }
+  }
+
+  onCopy(): void {
+    console.log('Copy clicked!');
+  }
 
 }
