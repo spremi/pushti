5026b9ff0e2adcdbdbef62b527ea8eee01c20902
Sanjeev Premi <spremi@ymail.com>
2023-09-03 12:26:01 +0530

    feat: parts: header: Make buttons route-aware
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/parts/header/header.component.ts
+++ src/app/parts/header/header.component.ts
@@ -8,13 +8,35 @@
 // (c) Copyright 2023 Sanjeev Premi.
 //
 
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { Subscription, filter } from 'rxjs';
 
 @Component({
   selector: 'sp-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.sass']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
 
+  private router = inject(Router);
+
+  private routerEvents$!: Subscription;
+
+  url = '';
+
+  ngOnInit(): void {
+    this.routerEvents$ = this.router.events.pipe(
+      filter((e): e is NavigationEnd => e instanceof NavigationEnd),
+    ).subscribe((e) => {
+      this.url = e.url;
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.routerEvents$) {
+      this.routerEvents$.unsubscribe();
+    }
+
+  }
 }
