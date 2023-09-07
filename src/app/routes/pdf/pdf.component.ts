5892de52c4ed95eac72688a046f35c707bc50a13
Sanjeev Premi <spremi@ymail.com>
2023-09-08 00:50:44 +0530

    feat: /pdf: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/routes/pdf/pdf.component.ts
+++ src/app/routes/pdf/pdf.component.ts
@@ -1,10 +1,92 @@
-import { Component } from '@angular/core';
+//
+// @project     pushti-web
+//
+// @author      Sanjeev Premi <spremi@ymail.com>
+//
+// @license     BSD-3-Clause
+//
+// (c) Copyright 2023 Sanjeev Premi.
+//
+
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { SafeResourceUrl } from '@angular/platform-browser';
+import { FileStoreService } from '@services/file-store.service';
+import { PdfParserService } from '@services/pdf-parser.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'sp-pdf',
   templateUrl: './pdf.component.html',
   styleUrls: ['./pdf.component.sass']
 })
-export class PdfComponent {
+export class PdfComponent implements OnInit, OnDestroy {
 
+  readonly ACCEPT_EXTENSIONS = [
+    '.pdf',
+  ].join(',');
+
+  private fileStore = inject(FileStoreService);
+  private snackBar = inject(MatSnackBar);
+  private pdfParser = inject(PdfParserService);
+
+  private pdfUpdate$!: Subscription;
+
+  pdfName = '';
+  pdfUrl!: SafeResourceUrl | null;
+
+  ngOnInit(): void {
+    this.pdfUpdate$ = this.pdfParser.updateObserver().subscribe(() => {
+      this.pdfName = this.pdfParser.getName();
+      this.pdfUrl = this.pdfParser.getPdf();
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.pdfUpdate$) {
+      this.pdfUpdate$.unsubscribe();
+    }
+  }
+
+  /**
+   * Handle the file select event.
+   */
+  onFileSelected(event: Event): void {
+    const target = event.target as HTMLInputElement;
+    const files = target.files as FileList;
+
+    // Attribute 'multiple' is set to false.
+    // We can safely use index 0 get file object.
+    const file = files[0];
+
+    const fileName = file.name;
+    const fileType = file.type;
+
+    console.log('Reading file ' + fileName + ' as ' + fileType);
+
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      let ret = this.fileStore.save(fileName, fileType, reader.result as string);
+
+      if (ret) {
+        this.pdfName = fileName;
+      } else {
+        this.snackBar.open('Unable to parse file: ' + fileName);
+      }
+    };
+
+    reader.onerror = () => {
+      this.snackBar.open('Unable to read file: ' + fileName + '\n' + reader.error?.message as string);
+    };
+
+    reader.readAsBinaryString(file);
+  }
+
+  onClear() {
+    this.pdfUrl = null;
+    this.pdfName = '';
+
+    this.fileStore.clear();
+  }
 }
