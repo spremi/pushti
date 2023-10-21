1a2e031c2fe1d3476d2913dac2c06d986003c42b
Sanjeev Premi <spremi@ymail.com>
2023-10-21 23:24:27 +0530

    feat: parts: filler: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/parts/filler/filler.component.ts
+++ src/app/parts/filler/filler.component.ts
@@ -1,4 +1,14 @@
-import { Component } from '@angular/core';
+//
+// @project     pushti-web
+//
+// @author      Sanjeev Premi <spremi@ymail.com>
+//
+// @license     BSD-3-Clause
+//
+// (c) Copyright 2023 Sanjeev Premi.
+//
+
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'sp-filler',
@@ -6,5 +16,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./filler.component.sass']
 })
 export class FillerComponent {
-
+  @Input() select!: string;
 }
