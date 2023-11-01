7ba5d67a06bb60e74e83ae94ad2fc818256f5945
Sanjeev Premi <spremi@ymail.com>
2023-11-01 15:42:38 +0530

    chore: app: Add class 'sp-pkcs7-content-id'
    Command:
      ng g class models/sp-pkcs7-content-id
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ src/app/models/sp-pkcs7-content-id.spec.ts
@@ -0,0 +1,7 @@
+import { SpPkcs7ContentId } from './sp-pkcs7-content-id';
+
+describe('SpPkcs7ContentId', () => {
+  it('should create an instance', () => {
+    expect(new SpPkcs7ContentId()).toBeTruthy();
+  });
+});
