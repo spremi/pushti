7b7c89022853d4a3258f2bd22c2697d8e50c79ea
Sanjeev Premi <spremi@ymail.com>
2023-11-01 15:41:29 +0530

    feat: models: Define 'SpOidPkcs'
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/models/sp-oid-pkcs.ts
+++ src/app/models/sp-oid-pkcs.ts
@@ -1,2 +1,32 @@
-export interface SpOidPkcs {
+//
+// @project     pushti-web
+//
+// @author      Sanjeev Premi <spremi@ymail.com>
+//
+// @license     BSD-3-Clause
+//
+// (c) Copyright 2023 Sanjeev Premi.
+//
+
+export const SpOidPkcs: Record<string, string> = {
+  '1.2.840.113549.1.2': 'PKCS #2',
+  '1.2.840.113549.1.3': 'PKCS #3',
+  '1.2.840.113549.1.4': 'PKCS #4',
+  '1.2.840.113549.1.5': 'PKCS #5',
+  '1.2.840.113549.1.6': 'PKCS #6',
+  '1.2.840.113549.1.7': 'PKCS #7',
+  '1.2.840.113549.1.8': 'PKCS #8',
+  '1.2.840.113549.1.9': 'PKCS #9',
+  '1.2.840.113549.1.10': 'PKCS #10: Certification Request Syntax',
+  '1.2.840.113549.1.11': 'Cryptographic Token Interface Standard',
+  '1.2.840.113549.1.12': 'PKCS #12',
+  '1.2.840.113549.1.15': '',
+  '1.2.840.113549.1.16': 'S/MIME (Secure Multipurpose Internet Mail Extensions)',
+
+  '1.2.840.113549.1.7.1': '(PKCS) #7 Data',
+  '1.2.840.113549.1.7.2': '(PKCS) #7 Signed Data',
+  '1.2.840.113549.1.7.3': '(PKCS) #7 Enveloped Data',
+  '1.2.840.113549.1.7.4': '(PKCS) #7 Signed &amp; Enveloped Data',
+  '1.2.840.113549.1.7.5': '(PKCS) #7 Digested Data',
+  '1.2.840.113549.1.7.6': '(PKCS) #7 Encrypted Data',
 }
