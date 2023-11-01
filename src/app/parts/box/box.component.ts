7262f4297bf971760b38f924f2d373ff9c6cb730
Sanjeev Premi <spremi@ymail.com>
2023-11-01 15:16:31 +0530

    feat: parts: box: Animate 'copy' icon on click
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/parts/box/box.component.ts
+++ src/app/parts/box/box.component.ts
@@ -8,9 +8,10 @@
 // (c) Copyright 2023 Sanjeev Premi.
 //
 
-import { Component, ElementRef, EventEmitter, Input, Output, inject } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, Output, ViewChild, inject } from '@angular/core';
 
 import { Clipboard } from '@angular/cdk/clipboard';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'sp-box',
@@ -24,10 +25,20 @@ export class BoxComponent {
   @Input() extnCritical = false;
   @Output() copy = new EventEmitter<any>();
 
+  @ViewChild('copyIcon') icon!: MatIcon;
+
   private elRef = inject(ElementRef);
   private clipboard = inject(Clipboard);
 
   onCopyContent(): void {
+    const iconElem = this.icon._elementRef.nativeElement;
+
+    iconElem.classList.add('ring');
+
+    setTimeout(() => {
+      iconElem.classList.remove('ring');
+    }, 1000);
+
     const text = this.elRef.nativeElement.innerText;
 
     // Remove instances of 'content_copy' (name of the icon)
