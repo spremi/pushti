711f7c334e6ea173cc4df0e46430372d9f1d7f7e
Sanjeev Premi <spremi@ymail.com>
2023-11-01 15:54:09 +0530

    chore: app: Add class 'sp-pkcs7-signature'
    Command:
      ng g class models/sp-pkcs7-signature
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ src/app/models/sp-pkcs7-signature.spec.ts
@@ -0,0 +1,7 @@
+import { SpPkcs7Signature } from './sp-pkcs7-signature';
+
+describe('SpPkcs7Signature', () => {
+  it('should create an instance', () => {
+    expect(new SpPkcs7Signature()).toBeTruthy();
+  });
+});
