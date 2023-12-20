e016dd7dc615e45eee89efdd4587771bf54c3d5d
Sanjeev Premi <spremi@ymail.com>
2023-12-20 22:54:28 +0530

    feat: parts: show-pdf-sign-container: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/parts/show-pdf-sign-container/show-pdf-sign-container.component.ts
+++ src/app/parts/show-pdf-sign-container/show-pdf-sign-container.component.ts
@@ -1,4 +1,15 @@
-import { Component } from '@angular/core';
+//
+// @project     pushti-web
+//
+// @author      Sanjeev Premi <spremi@ymail.com>
+//
+// @license     BSD-3-Clause
+//
+// (c) Copyright 2023 Sanjeev Premi.
+//
+
+import { Component, Input, SimpleChanges } from '@angular/core';
+import { SpPkcs7ContentId } from '@models/sp-pkcs7-content-id';
 
 @Component({
   selector: 'sp-show-pdf-sign-container',
@@ -6,5 +17,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./show-pdf-sign-container.component.sass']
 })
 export class ShowPdfSignContainerComponent {
+  @Input() value: SpPkcs7ContentId | null = null;
 
+  ngOnChanges(changes: SimpleChanges): void {
+    for (const attr in changes) {
+      if (attr === 'value') {
+        this.value = changes[attr].currentValue;
+
+        if (this.value) {
+          this.value = this.value;
+        } else {
+          this.value = null;
+        }
+      }
+    }
+  }
 }
