334a10a7643967851e0600e5c3fcdebe169d79eb
Sanjeev Premi <spremi@ymail.com>
2023-12-21 14:20:33 +0530

    feat: svc: cache: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/services/cache.service.ts
+++ src/app/services/cache.service.ts
@@ -1,9 +1,41 @@
+//
+// @project     pushti-web
+//
+// @author      Sanjeev Premi <spremi@ymail.com>
+//
+// @license     BSD-3-Clause
+//
+// (c) Copyright 2023 Sanjeev Premi.
+//
+
 import { Injectable } from '@angular/core';
 
+/**
+ * Temporary storage for certificates found in a PDF.
+ * Enables faster decoding...
+ */
 @Injectable({
   providedIn: 'root'
 })
 export class CacheService {
 
+  certificates: string[] = [];
+
   constructor() { }
+
+  add(cert: string): void {
+    this.certificates.push(cert);
+  }
+
+  get(index: number): string {
+    if (index >= 0 && index < this.certificates.length) {
+      return this.certificates[index];
+    }
+
+    return '';
+  }
+
+  clear(): void {
+    this.certificates = [];
+  }
 }
