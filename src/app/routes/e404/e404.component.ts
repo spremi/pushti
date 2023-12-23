2b69c610d2ea3bf972ea9d14652befccf0bf4ddb
Sanjeev Premi <spremi@ymail.com>
2023-12-23 20:36:11 +0530

    feat: /e404: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/routes/e404/e404.component.ts
+++ src/app/routes/e404/e404.component.ts
@@ -1,4 +1,15 @@
-import { Component } from '@angular/core';
+//
+// @project     pushti-web
+//
+// @author      Sanjeev Premi <spremi@ymail.com>
+//
+// @license     BSD-3-Clause
+//
+// (c) Copyright 2023 Sanjeev Premi.
+//
+
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'sp-e404',
@@ -7,4 +18,9 @@ import { Component } from '@angular/core';
 })
 export class E404Component {
 
+  private router = inject(Router);
+
+  go(): void {
+    this.router.navigate(['/home'], { replaceUrl: true });
+  }
 }
