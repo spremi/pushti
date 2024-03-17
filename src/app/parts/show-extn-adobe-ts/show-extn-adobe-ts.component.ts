dbc77bbe52b1d2b5d2047ecf7bd893de332f83cf
Sanjeev Premi <spremi@ymail.com>
2024-03-17 22:42:18 +0530

    feat: parts: show-extensions: Show Adobe Timestamp
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/parts/show-extn-adobe-ts/show-extn-adobe-ts.component.ts
+++ src/app/parts/show-extn-adobe-ts/show-extn-adobe-ts.component.ts
@@ -1,3 +1,13 @@
+//
+// @project     pushti-web
+//
+// @author      Sanjeev Premi <spremi@ymail.com>
+//
+// @license     BSD-3-Clause
+//
+// (c) Copyright 2023 Sanjeev Premi.
+//
+
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { SpX509ExtnAdobeTs } from '@models/sp-x509-extn-adobe-ts';
 
