b8555f18aa7af05f06d6f16738930a7b7618ba04
Sanjeev Premi <spremi@ymail.com>
2024-03-17 22:52:17 +0530

    chore: app: Add class 'sp-x509-extn-adobe-rev'
    Command:
      ng g class models/sp-x509-extn-adobe-rev
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ src/app/models/sp-x509-extn-adobe-rev.spec.ts
@@ -0,0 +1,7 @@
+import { SpX509ExtnAdobeRev } from './sp-x509-extn-adobe-rev';
+
+describe('SpX509ExtnAdobeRev', () => {
+  it('should create an instance', () => {
+    expect(new SpX509ExtnAdobeRev()).toBeTruthy();
+  });
+});
