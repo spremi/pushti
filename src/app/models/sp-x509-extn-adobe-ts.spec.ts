7864427ebf025a56324619af9ff84c5c72643851
Sanjeev Premi <spremi@ymail.com>
2024-03-17 19:22:08 +0530

    chore: app: Add class 'SpX509ExtnAdobeTs'
    Command:
      ng g class models/SpX509ExtnAdobeTs
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ src/app/models/sp-x509-extn-adobe-ts.spec.ts
@@ -0,0 +1,7 @@
+import { SpX509ExtnAdobeTs } from './sp-x509-extn-adobe-ts';
+
+describe('SpX509ExtnAdobeTs', () => {
+  it('should create an instance', () => {
+    expect(new SpX509ExtnAdobeTs()).toBeTruthy();
+  });
+});
