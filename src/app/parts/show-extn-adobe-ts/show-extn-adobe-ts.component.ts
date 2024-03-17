6be85d4e8e10e9ea898d249aa1152e4564a5e764
Sanjeev Premi <spremi@ymail.com>
2024-03-17 22:41:02 +0530

    feat: parts: show-extn-adobe-ts: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/parts/show-extn-adobe-ts/show-extn-adobe-ts.component.ts
+++ src/app/parts/show-extn-adobe-ts/show-extn-adobe-ts.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { SpX509ExtnAdobeTs } from '@models/sp-x509-extn-adobe-ts';
 
 @Component({
   selector: 'sp-show-extn-adobe-ts',
   templateUrl: './show-extn-adobe-ts.component.html',
   styleUrls: ['./show-extn-adobe-ts.component.sass']
 })
-export class ShowExtnAdobeTsComponent {
+export class ShowExtnAdobeTsComponent implements OnChanges {
+  @Input() oid = '';
+  @Input() critical = false;
+  @Input() value!: Uint8Array;
 
+  url: Uint8Array | null = null;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    for (const attr in changes) {
+      if (attr === 'value') {
+        this.value = changes[attr].currentValue;
+
+        if (this.value) {
+          const adobeTs = new SpX509ExtnAdobeTs();
+          adobeTs.setValue(this.value);
+
+          this.url = adobeTs.getUrl();
+        } else {
+          this.url = null;
+        }
+      }
+    }
+  }
+
+  onCopy(): void {
+  }
 }
