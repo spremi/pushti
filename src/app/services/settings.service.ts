163b49c1c7de61b8092e6ab63d3e22e4c937778c
Sanjeev Premi <spremi@ymail.com>
2024-04-01 02:49:35 +0530

    feat: svc: settings: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/services/settings.service.ts
+++ src/app/services/settings.service.ts
@@ -1,3 +1,13 @@
+//
+// @project     pushti-web
+//
+// @author      Sanjeev Premi <spremi@ymail.com>
+//
+// @license     BSD-3-Clause
+//
+// (c) Copyright 2023 Sanjeev Premi.
+//
+
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +15,62 @@ import { Injectable } from '@angular/core';
 })
 export class SettingsService {
 
+  private debug = false;
+  private allKeyUsages = false;
+
   constructor() { }
+
+  /**
+   * Read all settings from local storage.
+   */
+  init(): void {
+    this.debug = this.getBoolean('debug', false);
+    this.allKeyUsages = this.getBoolean('allKeyUsages', true);
+  }
+
+  /**
+   * Save all settings in local storage.
+   */
+  save(): void {
+    this.setBoolean('debug', this.debug);
+    this.setBoolean('allKeyUsages', this.allKeyUsages);
+  }
+
+  getDebug(): boolean {
+    return this.debug;
+  }
+
+  setDebug(flag: boolean): void {
+    this.debug = flag;
+  }
+
+  getAllKeyUsages(): boolean {
+    return this.allKeyUsages;
+  }
+
+  setAllKeyUsages(flag: boolean): void {
+    this.allKeyUsages = flag;
+  }
+
+  /**
+   * Read boolean value from local storage.
+   */
+  private getBoolean(key: string, alt: boolean): boolean {
+    const flag = localStorage.getItem(key);
+
+    if (flag == null) {
+      return alt;
+    }
+
+    return flag == 'true';
+  }
+
+  /**
+   * Save boolean value to local storage.
+   */
+  private setBoolean(key: string, value: boolean): void {
+    const flag = (value ? 'true' : 'false');
+
+    localStorage.setItem(key, flag);
+  }
 }
