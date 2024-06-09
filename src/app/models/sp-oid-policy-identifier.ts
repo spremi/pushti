ba55dc260af38eb908d0bf14188937e67fb23a81
Sanjeev Premi <spremi@ymail.com>
2024-06-09 19:13:31 +0530

    feat: models: oid-policy-identifier: Add oid definitions
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/models/sp-oid-policy-identifier.ts
+++ src/app/models/sp-oid-policy-identifier.ts
@@ -9,5 +9,13 @@
 //
 
 export const SpOidPolicyIdentifier: Record<string, string> = {
+  '0.4.0.2042.1.1': 'NCP (Normalized Certificate Policy)',
+  '0.4.0.2042.1.2': 'NCP+ (Normalized Certificate Policy requiring a secure user device)',
+  '0.4.0.2042.1.3': 'LCP (Lightweight Certificate Policy)',
+  '0.4.0.2042.1.4': 'EVCP (Extended Validation Certificate Policy)',
+  '0.4.0.2042.1.5': 'EVCP+ (Extended Validation Certificate Policy requiring a secure user device)',
+
   '2.5.29.32.0': 'Any Policy',
+
+  '2.16.840.1.114412.3.2': 'Object signing certificate',
 };
